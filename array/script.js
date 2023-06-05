9f2c50c8c07f67847b5fb7aa3986be0739087fcc
Abhishek kumar rawat <119392775+Abhirawat96@users.noreply.github.com>
2023-06-05 23:18:18 +0530

    Add files via upload

---
+++ array/script.js
@@ -0,0 +1,12 @@
+// //-----------------Array------------------------//
+
+
+// // var list =["tiger","cat", "dog", "mouse"]
+// // console.log(list[0]);
+
+// // var list =["tiger", "dog", "cat"];
+
+// // console.log(list[0] [1]);
+
+// var mylist=["dog","cat","bat","rat"];
+// var mylist2=mylist.concat(["monkey","tiger"]);
