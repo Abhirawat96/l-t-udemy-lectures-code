656cd239e5db82077c161e4424f21c3b0d466bc7
Abhishek kumar rawat <119392775+Abhirawat96@users.noreply.github.com>
2023-06-06 12:42:16 +0530

    Add files via upload

---
+++ js terminology/script.js
@@ -0,0 +1,45 @@
+//function declaration//
+
+function myfunction(){
+
+}
+
+
+//function expression//
+var newfunction = function(){
+
+};
+
+
+//function declaration also can have name//
+var newfunction = function myfunction(){
+
+};
+
+
+///expression////
+1+3;
+var a =2;
+return true;
+
+//callimg and  invoking a function
+alert()
+newfunction(param1 .param2);
+
+//asign a varaible.
+
+var a = true;
+
+//function vs method//
+function thisIsAFunction(){
+
+}
+
+
+var obj ={
+    thisisamethod:function(){
+
+    }
+}
+thisIsAFunction()
+obj.thisisamethod()
