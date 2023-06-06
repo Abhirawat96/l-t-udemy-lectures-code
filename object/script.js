b2fc6466233163826c3af9d502515ea3d87a5161
Abhishek kumar rawat <119392775+Abhirawat96@users.noreply.github.com>
2023-06-06 11:12:44 +0530

    Add files via upload

---
+++ object/script.js
@@ -0,0 +1,22 @@
+var user = {
+    nme: "sam",
+    age:34,
+    hobby:"gamer",
+    ismarried:false,
+    spells:["abrkabadraa","shazam","boo"],
+
+    shout: function(){
+        console.log("hiiiii");
+    }
+
+};
+// var list =["apple" , "banana" ,"oranfe"];
+var list = [{
+username:"abhi",
+password:"rawat"},
+{
+username:"bhai",
+password:"bro"
+}
+
+];
