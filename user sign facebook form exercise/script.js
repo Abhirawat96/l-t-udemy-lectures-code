5c3e2c9ef9ecebfcd2f065091593d0d4db21e1df
Abhishek kumar rawat <119392775+Abhirawat96@users.noreply.github.com>
2023-06-06 12:07:06 +0530

    Add files via upload

---
+++ user sign facebook form exercise/script.js
@@ -0,0 +1,103 @@
+// var database =[
+//     {
+//         username:"abhishek",
+//         password:"rawat"
+//     },
+// ];
+
+// var newsfeed = [{
+
+//     username:"vikas",
+//    timeline:"I am learning as much i can"
+// },
+
+// {
+//     username:"sammy",
+//   timeline:"i am giving my full time to code"
+// }
+// ];
+
+
+
+// var userNamePrompt =prompt("what is username");
+// var passWordPrompt =prompt("what is passwrod");
+
+
+// function signIn(user, pass){
+//     if(user === database[0].username && pass === 
+//         database[0].password){
+//             console.log(newsfeed);
+//         } else {
+//             alert("sorry incorrect username password");
+//         }
+    
+// }
+// signIn(userNamePrompt,passWordPrompt);
+
+// var database1 =[
+//     {
+//         userName:"userone",
+//         password:"passwprd"
+//     },
+// ]
+
+// var newsfeed =[
+//     {
+//         userName:"useronly",
+//         timeLine:"hello i am theuser one"
+//     },
+
+//     {
+//         userName:"useronly2",
+//         timeLine:"hello i am the user two"
+//     }
+// ];
+
+// var userNamePrompt = prompt("who is user");
+// var passWordPrompt = prompt("what's the password");
+ 
+
+// function signin(user,passwrod){
+//     if(user === database1[0],pass === database1[0]){
+//         console.log(newsfeed);
+//     } else {
+//         alert("enter correct password")
+//     }
+
+// } 
+
+// signin(userNamePrompt.passWordPrompt);
+
+
+var database = [
+    {
+        username:"user",
+        password:"password"
+    },
+]
+
+var Newsfeed = [
+    {
+        userone:"abhi",
+        timeline:"this is the user"
+        
+    },
+    {
+        usertwo:"vikas",
+        timline:"thisis user two"
+    },
+]
+
+var userNamePrompt = prompt("whois user");
+var passWordPrompt =prompt("what is passsowrd");
+
+
+function signin(user, pass){
+    if(user === database[0],pass === database[0]){
+        console.log(Newsfeed);
+    }else{
+        alert("enter correct detail");
+    }
+}
+
+signin(userNamePrompt,passWordPrompt);
